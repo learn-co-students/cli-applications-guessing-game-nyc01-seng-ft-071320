569de2dd0f24b8a73591ca07dd21ea248d56da8a
james wu <jwu_nyc@yahoo.com>
2020-07-06 18:35:34 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,17 @@
 # Code your solution here!
+def run_guessing_game
+  num=rand(1..6)
+  puts num
+  puts "Please guess"
+  input=gets.strip
+  x=input.to_i
+  puts="Please input exit to end the game"
+  if input =="exit"
+    puts "Goodbye!"
+  # elsif x.between?(1,6)
+
+
+  end
+end
+
+p run_guessing_game
